ca1123c825ca0d459445b1b0f91c7a0d5fc0c82d
kmeurink <kester.meurink@gmail.com>
2019-04-04 15:55:19 +0200

    Added methods for client and server to find and communicate with eachother.

---
+++ src/main/java/com/nedap/university/FileServer_ClientSide.java
@@ -0,0 +1,106 @@
+package com.nedap.university;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.util.Random;
+
+public class FileServer_ClientSide {
+//Named variables:
+	private static int clientPort = 8090;//TODO add way for client to set own port.
+	private static int serverPort = 8080;
+    private DatagramSocket socket;
+
+	
+	
+	
+    public FileServer_ClientSide(int port) throws SocketException {
+        socket = new DatagramSocket(port);
+    }
+    
+    public static void main(String[] args) {
+
+    	FileServer_ClientSide testClient;
+
+
+
+        try {//Setup connection with server
+			testClient = new FileServer_ClientSide(clientPort);
+            testClient.broadcastConnect();
+            testClient.service();
+        } catch (SocketException e) { //TODO needs handling
+			e.printStackTrace();
+		} catch (IOException ex) {
+            System.out.println("Client error: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+    /**
+     * Broadcasts a packet into the network to find its file server.
+     * @param socket
+     */
+    public void broadcastConnect() { //TODO testing method.
+    	//Status: broadcast is visible on wireshark
+    	
+    	//Test packet with only basic header
+    	byte[] testPacket = new byte[13];
+    	//Seq. number:
+    	testPacket[0] = 0;
+    	testPacket[1] = 0;
+    	testPacket[2] = 0;
+    	testPacket[3] = 5;
+    	//Ack. number:
+    	testPacket[4] = 0;
+    	testPacket[5] = 0;
+    	testPacket[6] = 0;
+    	testPacket[7] = 10;
+    	//Flags and commands:
+    	testPacket[8] = 0;
+    	//Window size:
+    	testPacket[9] = 0;
+    	testPacket[10] = 0;
+    	//Checksum:
+    	testPacket[11] = 0;
+    	testPacket[12] = 0;
+    	
+    	//Broadcast packet to find server.
+    	InetAddress broadcast;
+		try {
+			broadcast = InetAddress.getByName("localhost");//TODO testing using localhost "255.255.255.255");
+			DatagramPacket request = new DatagramPacket(testPacket, 13, broadcast, serverPort);
+				socket.send(request);        
+		} catch (UnknownHostException e) {//TODO handle better
+			e.printStackTrace();
+		} catch (IOException e) {//TODO handle better
+			e.printStackTrace();
+		}
+
+    }
+    
+    /**
+     * Class to receive and print all packets during its connection.
+     * @throws IOException
+     */
+    private void service() throws IOException {
+        while (true) {
+        	//Receive packet from client.
+            DatagramPacket request = new DatagramPacket(new byte[13], 13);
+            socket.receive(request);
+            byte[] tempData = request.getData();
+            for (byte i : tempData) {
+                System.out.println(Byte.toString(i)); //TODO for testing.
+
+            }
+            
+            //Get address and port of client from the request.
+            InetAddress clientAddress = request.getAddress();
+            int clientPort = request.getPort();
+
+        }
+    }
+    
+}
---
+++ src/main/java/com/nedap/university/FileServer_ServerSide.java
@@ -0,0 +1,103 @@
+package com.nedap.university;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FileServer_ServerSide {
+
+    private DatagramSocket socket;
+    private List<String> listQuotes = new ArrayList<String>();
+    private Random random;
+	private static int serverPort = 8080;
+
+    public FileServer_ServerSide(int port) throws SocketException {
+        socket = new DatagramSocket(port);
+        random = new Random();
+    }
+ 
+    public static void main(String[] args) {
+    	//Requires specific file with quotes, and port to listen on.
+    	//TODO specific file is not necessary for file server, only folder where files can be found.
+ 
+ 
+        try {
+        	FileServer_ServerSide server = new FileServer_ServerSide(serverPort);
+            server.service();
+        } catch (SocketException ex) {//TODO handle better
+            System.out.println("Socket error: " + ex.getMessage());
+        } catch (IOException ex) {//TODO handle better
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
+    
+    /**
+     * Class to run all required actions during its connection.
+     * @throws IOException
+     */
+    private void service() throws IOException {
+        while (true) {
+        	//Receive packet from client.
+            DatagramPacket request = new DatagramPacket(new byte[13], 13);
+            socket.receive(request);
+            byte[] tempData = request.getData();
+            for (byte i : tempData) {
+                System.out.println(Byte.toString(i)); //TODO for testing.
+
+            }
+            
+            //Get address and port of client from the request.
+            InetAddress clientAddress = request.getAddress();
+            int clientPort = request.getPort();
+            
+          //Test packet with only basic header
+        	byte[] testPacket = new byte[13];
+        	//Seq. number:
+        	testPacket[0] = 0;
+        	testPacket[1] = 0;
+        	testPacket[2] = 0;
+        	testPacket[3] = 6;
+        	//Ack. number:
+        	testPacket[4] = 0;
+        	testPacket[5] = 0;
+        	testPacket[6] = 0;
+        	testPacket[7] = 20;
+        	//Flags and commands:
+        	testPacket[8] = 0;
+        	//Window size:
+        	testPacket[9] = 0;
+        	testPacket[10] = 0;
+        	//Checksum:
+        	testPacket[11] = 0;
+        	testPacket[12] = 0;
+        	
+            //Send packet with chosen data to the client.
+            DatagramPacket response = new DatagramPacket(testPacket, testPacket.length, clientAddress, clientPort);
+            socket.send(response);
+            System.out.println("response sent."); //TODO for testing.
+        }
+    }
+ 
+    //Load the quotes from a file into a string array.
+    //TODO replace by loading a folder.
+    private void loadQuotesFromFile(String quoteFile) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(quoteFile));
+        String aQuote;
+ 
+        while ((aQuote = reader.readLine()) != null) {
+            listQuotes.add(aQuote);
+        }
+ 
+        reader.close();
+    }
+ 
+
+
+}
