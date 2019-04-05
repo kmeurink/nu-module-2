c238a6c41d59b7fb193e12d70f9b1a60630a66e7
kmeurink <kester.meurink@gmail.com>
2019-04-05 16:09:01 +0200

    Working on packetSender and packetReceiver, added send and receive methods. And the secondary methods/queries associated with them

--- src/main/java/com/nedap/university/communication/PacketReceiver.java
+++ src/main/java/com/nedap/university/communication/PacketReceiver.java
@@ -1,20 +1,69 @@
 package com.nedap.university.communication;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 /**
  * Receives the packets from the server or client.
  * @author kester.meurink
  *
  */
 public class PacketReceiver {//TODO perhaps better as a method, but is used by both server and client. Also has to be given the socket to work.
+	
 	//Named Constants:
+	private DatagramSocket socket;
+	private int packetSize = 512;//TODO remove eventually
+	private int receiverPort;
+	private InetAddress receiverAddress;
+	private byte[] packetData;
 	
 	//Constructors:
-	
+	public PacketReceiver(DatagramSocket socket) {
+		this.socket = socket;
+		this.packetData = new byte[512];
+	}
 	
 	//Queries:
+	/**
+	 * Returns the port of the sender of the last packet.
+	 * @return
+	 */
+	public int getReceiverPort() {
+		return this.receiverPort;
+	}
 	
 	
+	/**
+	 * Returns the address of the sender of the last packet.
+	 * @return
+	 */
+	public InetAddress getReceiverAddress() {
+		return this.receiverAddress;
+	}
 	
 	//Commands:
+	  /**
+     * Class to receive any packets sent to the socket.
+     * @throws IOException
+     */
+    public void receivePacket() throws IOException { //TODO rewrite method, is messy
+        //while (true) {
+        	//Receive packet from client.
+            DatagramPacket request = new DatagramPacket(new byte[packetSize], packetSize);
+            socket.receive(request);
+            this.packetData = request.getData();
+            for (byte i : packetData) {
+                System.out.println(Byte.toString(i)); //TODO for testing.
 
+            }
+            
+            //Get address and port of client from the request.
+            this.receiverAddress = request.getAddress();
+            this.receiverPort = request.getPort();
+            //TODO add perhaps a notify that notifies the handler to do something with the received packet.
+        //}
+    }
+    
+    
 }
--- src/main/java/com/nedap/university/communication/PacketSender.java
+++ src/main/java/com/nedap/university/communication/PacketSender.java
@@ -1,5 +1,10 @@
 package com.nedap.university.communication;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 /**
  * Sends the packets to the server or client.
  * @author kester.meurink
@@ -7,14 +12,42 @@ package com.nedap.university.communication;
  */
 public class PacketSender { //TODO perhaps better as a method, but is used by both server and client. Also has to be given the socket to work.
 	//Named Constants:
+    private DatagramSocket socket;
+    public static InetAddress BROADCAST;
+    private InetAddress serverAddress;
+    private DatagramPacket packetToSend;
 	
 	//Constructors:
-	
+	public PacketSender(DatagramSocket socket) throws UnknownHostException {
+		this.socket = socket;
+		this.BROADCAST = InetAddress.getByName("255.255.255.255");
+	}
 	
 	//Queries:
 	
 	
 	
 	//Commands:
-
+	
+	/**
+	 * Builds the datagram packet that is to be sent to the receiver.
+	 * @param receiver
+	 * @param receiverPort
+	 */
+	public void buildDatagram(InetAddress receiver, int receiverPort, byte[] packet) {
+		packetToSend = new DatagramPacket(packet, packet.length, receiver, receiverPort);
+		
+	}
+	
+	/**
+	 * Sends a datagram packet to a receiver. 
+	 * @param packet
+	 */
+	public void sendPacket(DatagramPacket packet) { //TODO perhaps remove the argument, as it will always be the packetToSend
+		try {
+			socket.send(packet);
+		} catch (IOException e) { //TODO add handling of error
+			e.printStackTrace();
+		}
+	}
 }
