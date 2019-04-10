ea731b5644f9ff79432279aa1d0660acf6ec3c58
kmeurink <kester.meurink@gmail.com>
2019-04-10 13:36:02 +0200

    Rewrote the inputhandler to create a better structure. Now working on building the methods called inside the switch case.

--- src/main/java/com/nedap/university/communication/InputHandler.java
+++ src/main/java/com/nedap/university/communication/InputHandler.java
@@ -1,5 +1,13 @@
 package com.nedap.university.communication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.nedap.university.utilities.InputCommands;
+
 /**
  * Deals with the packet header to identify the purpose and contents of a packet.
  * @author kester.meurink
@@ -10,10 +18,13 @@ public class InputHandler {//TODO perhaps better as a method, but is used by bot
 	private PacketBuilder inputPacket;
 	private int dataSize = 1011; //TODO make changeable eventually
 	private int packetSize = 1024;//TODO make changeable eventually
-	
+    private InetAddress BROADCAST;
+    private InputCommands commands;
 	//Constructors:
-	public InputHandler() {
-		inputPacket = new PacketBuilder(dataSize, packetSize) ;
+	public InputHandler() throws UnknownHostException {
+		inputPacket = new PacketBuilder(dataSize, packetSize);
+		this.BROADCAST = InetAddress.getByName("255.255.255.255");
+		commands = new InputCommands();
 	}
 	
 	//Queries:
@@ -28,86 +39,83 @@ public class InputHandler {//TODO perhaps better as a method, but is used by bot
 	 * Reads out the contents of the packet and determines what to do.
 	 * @param packet
 	 */
-	public void PacketInputHandler(byte[] packet) { //TODO determine if the current setup is correct.
+	public void PacketInputSort(byte[] packet, InetAddress addr, int port) { //TODO determine if the current setup is correct.
+		List<byte[]> dataList = new ArrayList<byte[]>();
 		inputPacket.setPacket(packet);
+		if (Arrays.equals(inputPacket.calculateCheckSum(packet), inputPacket.getCheckSum())) {
+			
+		
 		int command = (int) inputPacket.getFlags();
 		switch(command) { //TODO add actions
-		case (byte) 1: //SYN
+		//List function options:
+		case (byte) 33: //SYN/LIST
+			dataList = commands.listRequest();
+			break;
+		case (byte) 35: //SYN/LIST/ACK
 			
 			break;
-		case (byte) 2: //ACK
+		case (byte) 39: //SYN/LIST/ACK/FIN
 			
 			break;
-		case (byte) 4: //FIN
+		case (byte) 34: //LIST/ACK
 			
 			break;
-		case (byte) 8: //UPLOAD
+		//Pause function options:
+		case (byte) 65: //PAUSE/SYN
 			
 			break;
-		case (byte) 16: //DOWNLOAD
+		case (byte) 67: //PAUSE/SYN/ACK
 			
 			break;
-		case (byte) 32: //LIST
+		case (byte) 66: //PAUSE/ACK
 			
 			break;
-		case (byte) 64: //PAUSE
-			
-			break;
-		case (byte) -128: //START/STOP
-			
-			break;
-		case (byte) 3: //SYN/ACK
-			
-			break;
-		case (byte) 9: //SYN/UPLOAD
-			
-			break;
+		//Download function options:
 		case (byte) 17: //SYN/DOWNLOAD
 			
 			break;
-		case (byte) 33: //SYN/LIST
-					
-			break;
-		case (byte) 6: //ACK/FIN
-			
-			break;
-		case (byte) 10: //ACK/UPLOAD
+		case (byte) 19: //SYN/DOWNLOAD/ACK
 			
 			break;
 		case (byte) 18: //ACK/DOWNLOAD
 			
 			break;
-		case (byte) 34: //ACK/LIST
-			
-			break;
-		case (byte) 66: //ACK/PAUSE
-			
-			break;
-		case (byte) -126: //ACK/START_STOP
-			
-			break;
-		case (byte) 5: //SYN/FIN
-			
-			break;
-		case (byte) 12: //FIN/UPLOAD
+		case (byte) 16: //DOWNLOAD
 			
 			break;
 		case (byte) 20: //FIN/DOWNLOAD
 			
 			break;
-		case (byte) 36: //FIN/LIST
+		//Upload function options:
+		case (byte) 9: //SYN/UPLOAD
 			
 			break;
-		case (byte) 68: //FIN/PAUSE
+		case (byte) 11: //SYN/UPLOAD/ACK
 			
 			break;
-		case (byte) -124: //FIN/START_STOp
+		case (byte) 8: //UPLOAD
 			
 			break;
-		case (byte) 0: //no flags set, only used for a broadcast.
+		case (byte) 10: //UPLOAD/ACK
 			
 			break;
-		
+		case (byte) 12: //FIN/UPLOAD
+			
+			break;
+		case (byte) 14: //FIN/UPLOAD/ACK
+			
+			break;
+		//Stop function options:
+		case (byte) -127: //STARTSTOP/SYN
+			
+			break;
+		case (byte) -126: //STARTSTOP/ACK
+			
+			break;
+		case (byte) 0: //no flags set, only used for a broadcast.TODO is this useful?
+			
+			break;
+		}
 		}
 		
 	}
